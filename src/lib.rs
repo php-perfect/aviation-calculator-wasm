1e8792445f45bf5e5fb587f438eb02154381189a
Nico Löber <nico.loeber94@gmail.com>
2023-07-31 16:10:03 +0200

    Tidy up things and add release workflow

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use aviation_calculator::*;
+use aviation_calculator::fk9::{calculate_start_distance, Engine as LibEngine};
 use wasm_bindgen::prelude::*;
 
 mod utils;
@@ -10,16 +11,37 @@ mod utils;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub enum Engine {
+    Rotax912Ul,
+    Rotax912Uls,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, aviation-calculator-wasm!");
+pub struct TakeoffDistances {
+    pub takeoff_run: f64,
+    pub to_50_ft_height: f64,
+}
+
+#[wasm_bindgen]
+pub fn fk9_takeoff_distance(engine: Engine, mass: f64, pressure_altitude: f64, temperature: f64, slope: f64, wet_surface: bool, soft_surface: bool, high_gras: bool) -> TakeoffDistances {
+    utils::set_panic_hook();
+
+    let distances: (f64, f64) = calculate_start_distance(match engine {
+        Engine::Rotax912Ul => LibEngine::Rotax912Ul,
+        Engine::Rotax912Uls => LibEngine::Rotax912Uls,
+    }, mass, pressure_altitude, temperature, slope, wet_surface, soft_surface, high_gras).unwrap_throw();
+
+    TakeoffDistances {
+        takeoff_run: distances.0,
+        to_50_ft_height: distances.1,
+    }
 }
 
 #[wasm_bindgen]
 pub fn ground_speed(course: f64, tas: f64, wd: f64, ws: f64) -> f64 {
-    return calculate_ground_speed(course, tas, wd, ws);
+    utils::set_panic_hook();
+    calculate_ground_speed(course, tas, wd, ws)
 }
+
+#[cfg(test)]
+mod tests {}
--- tests/web.rs
+++ tests/web.rs
@@ -3,11 +3,16 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+
 use wasm_bindgen_test::*;
+
+use aviation_calculator_wasm::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
-    assert_eq!(1 + 1, 2);
+    let result: TakeoffDistances = fk9_takeoff_distance(Engine::Rotax912Uls, 520.0, 300.0, 1.0, 0.0, false, false, false);
+    assert_eq!(130.38, result.takeoff_run);
+    assert_eq!(323.48, result.to_50_ft_height);
 }
